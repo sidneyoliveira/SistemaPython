40d86e31d5526d2f03cf11ae18eddaa40dab0127
sidneyoliveira <sidneyoliveira2015@gmail.com>
2023-03-22 23:21:46 -0300

    a

--- oficios.py
+++ oficios.py
@@ -12,6 +12,12 @@ import subprocess
 from tkinter import *
 from PIL import Image, ImageTk
 
+
+
+cnpj1 = ""
+cnpj2 = ""
+cnpj3 = ""
+
 class CustomButton(customtkinter.CTkButton):
     def __init__(self, master, **kw):
         customtkinter.CTkButton.__init__(self, master=master, **kw)
@@ -19,37 +25,99 @@ class CustomButton(customtkinter.CTkButton):
         self.master.return_value = value
         self.master.destroy()
 
+
+
+
 def input_dados():
     root = customtkinter.CTk()
-    root.geometry("300x200")
+    root.geometry("600x300")
     root.title("Input Demo")
+
+    def on_input_change(var_name, input_obj):
+        def on_change(event):
+            globals()[var_name] = input_obj.get()
+
+        return on_change
 
     # Definindo a função que será executada quando o botão for clicado
     def input_pdf():
-        entry = filedialog.askopenfilename(title="Selecione o arquivo MAPA PDF", filetypes=(("pdf files", "*.pdf"),))
-        custom_button.return_value(entry)
+        try:
+            entry = filedialog.askopenfilename(title="Selecione o arquivo MAPA PDF", filetypes=(("pdf files", "*.pdf"),))
+            custom_button.return_value(entry)
+        except FileNotFoundError:
+            print("Arquivo não encontrado.")
+        except:
+            print("Ocorreu um erro inesperado.")
+        finally:
+            print("Importação do arquivo concluída.")
 
-    input_cnpj = customtkinter.CTkEntry(root)
-    input_cnpj.pack(pady=10)
-    print(input_cnpj)
+    input_cnpj1 = customtkinter.CTkEntry(root, placeholder_text="CNPJ",
+                                         width=200, height=40,
+                                         border_width=2,
+                                         border_color='#dddddd',
+                                         corner_radius=10, )
+    input_cnpj1.grid(column=1, row=1, padx=10, pady=10)
+    input_cnpj1.bind("<FocusOut>", on_input_change("cnpj1", input_cnpj1))
+
+
+    input_data1 = customtkinter.CTkEntry(root, placeholder_text="Data",
+                                         width=200, height=40,
+                                         border_width=2,
+                                         border_color='#dddddd',
+                                         corner_radius=10, )
+    input_data1.grid(column=2, row=1, padx=10, pady=10)
+    input_data1.bind("<FocusOut>", on_input_change("data1", input_data1))
+
+    input_cnpj2 = customtkinter.CTkEntry(root, placeholder_text="CNPJ",
+                                         width=200, height=40,
+                                         border_width=2,
+                                         border_color='#dddddd',
+                                         corner_radius=10, )
+
+    input_cnpj2.grid(column=1, row=2, padx=10, pady=10)
+    input_cnpj2.bind("<FocusOut>", on_input_change("cnpj2", input_cnpj2))
+
+    input_data2 = customtkinter.CTkEntry(root, placeholder_text="Data",
+                                         width=200, height=40,
+                                         border_width=2,
+                                         border_color='#dddddd',
+                                         corner_radius=10, )
+    input_data2.grid(column=2, row=2, padx=10, pady=10)
+    input_data2.bind("<FocusOut>", on_input_change("data2", input_data2))
+
+    input_cnpj3 = customtkinter.CTkEntry(root, placeholder_text="CNPJ",
+                                         width=200, height=40,
+                                         border_width=2,
+                                         border_color='#dddddd',
+                                         corner_radius=10, )
+    input_cnpj3.grid(column=1, row=3, padx=10, pady=10)
+    input_cnpj3.bind("<FocusOut>", on_input_change("cnpj3", input_cnpj3))
+
+    input_data3 = customtkinter.CTkEntry(root, placeholder_text="Data",
+                                         width=200, height=40,
+                                         border_width=2,
+                                         border_color='#dddddd',
+                                         corner_radius=10, )
+    input_data3.grid(column=2, row=3, padx=10, pady=10)
+    input_data3.bind("<FocusOut>", on_input_change("data3", input_data2))
 
     # Criando a entrada e o botão personalizado
-    custom_button = CustomButton(root, text="Abre o Mapa PDF", command=input_pdf)
-    custom_button.pack(pady=10)
+    custom_button = CustomButton(root, text="Abrir Mapa PDF", command=input_pdf)
+    custom_button.grid(column=1, row=4, padx=10, pady=10, sticky=W)
 
 
     # Iniciando a janela principal e esperando pelo retorno de valor
     root.mainloop()
 
-    # Retornando o valor após a janela ser fechada
     return root.return_value
 
 
 
-url_pdf = input_dados()
+dados = input_dados()
+print(dados)
 
 # Extrai texto do arquivo PDF PAGINA 1
-pag1 = extract_text(url_pdf, page_numbers=[0])
+pag1 = extract_text(dados, page_numbers=[0])
 
 # Divide o texto em linhas
 linhas = pag1.split("\n")
@@ -90,35 +158,31 @@ except Exception as e:
     print("Erro: " + str(e))
 
 #Função para extrair dados do cnpj
-def dados_cnpj():
-    cnpj_input = input('Qual CNPJ?')
-    data = input('Qual a Data?')
-    url = f'https://cnpj.biz/{cnpj_input}'
+def dados_cnpj(a):
+    url = f'https://cnpj.biz/{a}'
     response = requests.get(url)
     content = response.content
     dados_cnpj = BeautifulSoup(content, 'html.parser')
+    print(dados_cnpj)
+
     dados_cnpj = dados_cnpj.findAll('b', attrs={'class': 'copy'})
+    print(url)
+    print(dados_cnpj)
     cnpj = dados_cnpj[0].text
     razao = dados_cnpj[2].text
-    return cnpj, razao, data
+    return cnpj, razao
 
-empresa1 = dados_cnpj()
-cnpj1 = empresa1[0]
+empresa1 = dados_cnpj(cnpj1)
+cnpj1_input = empresa1[0]
 razao1 = empresa1[1]
-data1 = empresa1[2]
 
-print(cnpj1)
-print(razao1)
-
-empresa2 = dados_cnpj()
-cnpj2 = empresa2[0]
+empresa2 = dados_cnpj(cnpj2)
+cnpj2_input = empresa2[0]
 razao2 = empresa2[1]
-data2 = empresa2[2]
 
-empresa3 = dados_cnpj()
-cnpj3 = empresa3[0]
+empresa3 = dados_cnpj(cnpj3)
+cnpj3_input = empresa3[0]
 razao3 = empresa3[1]
-data3 = empresa3[2]
 
 
 numoficio = f'{dia}' + f'{mes}' + '-0001.' + f'{ano}'
@@ -149,9 +213,9 @@ p2.alignment = docx.enum.text.WD_ALIGN_PARAGRAPH.JUSTIFY
 table = doc.tables[0]
 
 referencias = {
-    "CNPJ1": f'{cnpj1}',
-    "CNPJ2": f'{cnpj2}',
-    "CNPJ3": f'{cnpj3}',
+    "CNPJ1": f'{cnpj1_input}',
+    "CNPJ2": f'{cnpj2_input}',
+    "CNPJ3": f'{cnpj3_input}',
     "RAZAO1": f'{razao1}',
     "RAZAO2": f'{razao2}',
     "RAZAO3": f'{razao3}',
@@ -173,7 +237,7 @@ for i in range(len(table.rows)):
 for para in doc.paragraphs:
     for run in para.runs:
         run.font.name = 'Calibri Light'
-
+print("final")
 
 doc.save(f'{numoficio}' + ' - ' + f'{titulo}' + '.docx')
 
